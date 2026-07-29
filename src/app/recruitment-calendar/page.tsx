6da81634dcb97525551be6e92a85503040b3756b
Yuhan Yuan <yyh0434782728@outlook.com>
2026-07-30 00:16:52 +1000

    feat: add verified recruitment calendar

---
+++ src/app/recruitment-calendar/page.tsx
@@ -0,0 +1,5 @@
+import { RecruitmentCalendar } from "@/components/recruitment-calendar/recruitment-calendar";
+
+export default function RecruitmentCalendarPage() {
+  return <RecruitmentCalendar />;
+}
